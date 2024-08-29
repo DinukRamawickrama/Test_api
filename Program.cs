9f49aed74192f489cbc6c8b59ad791b0cd1819db
DinukRamawickrama-github <dinukkaumika199@gmail.com>
2024-08-29 12:39:14 +0530

    EC2-DB

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddScoped<WebAPP.Services.IUserService, UserService>();
 
 
 
-builder.Services.AddDbContext<AppDbContext>(dbOptions => dbOptions.UseMySQL("Server = sqlserver.mysql.database.azure.com; Database = webapp; User Id = dkr; Password = SGxd2544; "));
+builder.Services.AddDbContext<AppDbContext>(dbOptions => dbOptions.UseMySQL("Server=52.23.160.92;Database=webapp;User Id=dkr;Password=SGxd2544;"));
 
 builder.Services.AddCors(options =>
 {
